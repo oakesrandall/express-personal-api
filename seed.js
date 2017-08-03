d4e2410ed3a05884fa00482bd98698a48c87fe5f
David_Randall <oakesrandall@gmail.com>
2017-08-03 15:42:24 -0600

    working

--- seed.js
+++ seed.js
@@ -1,15 +1,28 @@
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
-// var db = require('./models');
+var db = require('./models');
 
-// var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
+var newPet = [{
+    name: "Minnie",
+    species: "Dog",
+    breed: "Great Dane",
+}, {
+    name: "Dexter",
+    species: "Turtle",
+    breed: "Who the hell knows?",
+}];
 
-// db.Campsite.create(new_campsite, function(err, campsite){
-//   if (err){
-//     return console.log("Error:", err);
-//   }
-
-//   console.log("Created new campsite", campsite._id)
-//   process.exit(); // we're all done! Exit the program.
-// })
+db.Pet.remove({}, function(err, pets) {
+    if (err) {
+        console.log("Error: ", err);
+    }
+    console.log("removed all pets");
+    db.Pet.create(newPet, function(err, pet) {
+        if (err) {
+            return console.log("Error: ", err);
+        }
+        console.log("rebuilt pets");
+        process.exit();
+    });
+});
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
  * DATABASE *
  ************/
 
-// var db = require('./models');
+var db = require('./models');
 
 /**********
  * ROUTES *
@@ -26,8 +26,8 @@ app.use(express.static('public'));
  * HTML Endpoints
  */
 
-app.get('/', function homepage(req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 
@@ -35,19 +35,87 @@ app.get('/', function homepage(req, res) {
  * JSON API Endpoints
  */
 
-app.get('/api', function api_index(req, res) {
-  // TODO: Document all your api endpoints below
-  res.json({
-    woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
-    message: "Welcome to my personal api! Here's what you need to know!",
-    documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
-    endpoints: [
-      {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
-    ]
+app.get('/api', function (req, res) {
+    // TODO: Document all your api endpoints below
+    res.json({
+        message: "Welcome to my personal api! Here's what you need to know!",
+        documentation_url: "https://github.com/oakesrandall/express-personal-api/blob/master/README.md", // CHANGE ME
+        base_url: "https://damp-tundra-18179.herokuapp.com",
+        endpoints: [
+            { method: "GET", path: "/api", description: "Describes all available endpoints" },
+            { method: "GET", path: "/api/profile", description: "Data about me" },
+            { method: "POST", path: "/api/pets", description: "Create a new pet" }
+        ]
+    });
+});
+
+app.get('/api/profile', function (req, res) {
+    res.json({
+        name: 'David Randall',
+        github_link: 'https://github.com/oakesrandall',
+        github_profile_image: 'https://avatars0.githubusercontent.com/u/24509797?v=4&u=c5a0d0826c89780c4079e2128a92390d843c6dc3&s=400',
+        current_city: 'Colorado Springs'
+    });
+});
+
+app.post('/api/pets', function (req, res) {
+    // res.send(console.log('hit pets post route'));
+    // res.send(console.log('request: ' + req.body));
+    var newPet = new db.Pet({
+        name: req.body.name,
+        species: req.body.species,
+        breed: req.body.breed,
+    });
+    newPet.save(function(err, pet) {
+        if (err) {
+            return console.log('save error: ' + err);
+        }
+        console.log('saved ', pet.name);
+        res.json(pet);
+    });
+});
+
+app.get('/api/pets', function (req, res) {
+    db.Pet.find(function(err, pets) {
+        if (err) {
+            return (console.log('No pets to show'));
+        }
+        res.json(pets);
+    });
+});
+
+app.get('/api/pets/:name', function(req, res) {
+  db.Pet.findOne({name: req.params.name}, function(err, pet) {
+    console.log(req.params.name);
+    if (err) {
+      return console.log('that pet does not exist' + err);
+    }
+    res.json(pet);
   });
+});
+
+app.put('/api/pets/:name', function(req, res) {
+    console.log('hit put route for ' + req.params.name);
+    db.Pet.findOne({name: req.params.name}, function(err, pet) {
+        pet.name    = req.body.name;
+        pet.species = req.body.species;
+        pet.breed   = req.body.breed;
+        console.log(req.body.breed);
+        if (err) {
+            return console.log('that pet does not exist' + err);
+        } 
+        res.json(pet);
+    });
+});
+
+app.delete('/api/pets/:name', function(req, res) {
+    console.log('hit delete route, trying to delete ' + req.params.name);
+    db.Pet.findOneAndRemove({name: req.params.name}, function(err, deletedPet) {
+        if (err) {
+            return console.log('that pet does not exist' + err);
+        }
+        res.json(deletedPet);
+    });
 });
 
 /**********
@@ -55,6 +123,6 @@ app.get('/api', function api_index(req, res) {
  **********/
 
 // listen on port 3000
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Express server is up and running on http://localhost:3000/');
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Express server is up and running on http://localhost:3000/');
 });
